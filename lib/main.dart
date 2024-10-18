5cf6e6c92db7ea0027c0a44d4224ba9e3720ba8a
Dhafa <sucsespart1@gmail.com>
2024-10-18 22:12:21 +0700

    tambah komen setiap baris code

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,42 @@
+// Mengimpor paket-paket yang dibutuhkan
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 
+// Fungsi utama aplikasi, yang menjalankan aplikasi MyApp
 void main() => runApp(const MyApp());
 
+// Widget utama aplikasi yang merupakan StatelessWidget
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Scaffold menyediakan struktur dasar untuk halaman aplikasi
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Simple Calcuator'),
+          // Judul aplikasi di dalam AppBar
+          title: const Text('Simple Calculator'),
         ),
+        // Padding untuk memberikan jarak di sekeliling widget di dalamnya
         body: const Padding(
-            padding: EdgeInsets.all(18.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: CalcButton(),
-            )),
+          padding: EdgeInsets.all(18.0),
+          child: SizedBox(
+            // Mengatur lebar widget agar memenuhi lebar layar
+            width: double.infinity,
+            // Menampilkan widget CalcButton sebagai konten utama
+            child: CalcButton(),
+          ),
+        ),
       ),
     );
   }
 }
 
+// Widget CalcButton yang merupakan StatefulWidget
 class CalcButton extends StatefulWidget {
   const CalcButton({Key? key}) : super(key: key);
 
@@ -34,57 +44,72 @@ class CalcButton extends StatefulWidget {
   CalcButtonState createState() => CalcButtonState();
 }
 
+// State dari CalcButton yang menyimpan nilai dan menangani perubahan
 class CalcButtonState extends State<CalcButton> {
+  // Variabel untuk menyimpan nilai saat ini yang ditampilkan di kalkulator
   double? _curentValue = 0;
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    // Membuat instance dari SimpleCalculator dengan konfigurasi yang ditentukan
     var calc = SimpleCalculator(
-      value: _curentValue!,
-      hideExpression: false,
-      hideSurroundingBorder: true,
-      autofocus: true,
-      onChanged: (key, value, expression){
+      value: _curentValue!, // Nilai awal kalkulator
+      hideExpression: false, // Menampilkan ekspresi perhitungan
+      hideSurroundingBorder: true, // Menghilangkan border di sekitar kalkulator
+      autofocus: true, // Fokus otomatis pada kalkulator saat muncul
+      onChanged: (key, value, expression) {
+        // Callback ketika nilai kalkulator berubah
         setState(() {
-          _curentValue = value ?? 0;
+          _curentValue = value ?? 0; // Mengupdate nilai saat ini
         });
-        if(kDebugMode){
-          print ('$key\$value\t$expression');
+        if (kDebugMode) {
+          // Mencetak informasi ke konsol jika dalam mode debug
+          print('$key \$value\t$expression');
         }
       },
-      onTappedDisplay: (value, details){
-        if(kDebugMode){
+      onTappedDisplay: (value, details) {
+        // Callback ketika tampilan kalkulator diketuk
+        if (kDebugMode) {
+          // Mencetak nilai dan posisi ketukan ke konsol
           print('$value\t${details.globalPosition}');
         }
       },
       theme: const CalculatorThemeData(
-        borderColor: Colors.black,
-        borderWidth: 2,
-        displayColor: Colors.white,
-        expressionColor: Colors.indigo,
-        expressionStyle: TextStyle(fontSize: 80, color: Colors.yellow),
-        operatorColor: Colors.pink,
-        operatorStyle: TextStyle(fontSize: 30, color: Colors.white),
-        commandColor: Colors.orange,
-        commandStyle: TextStyle(fontSize: 30, color: Colors.white),
-        numColor: Colors.grey,
-        numStyle: TextStyle(fontSize: 50, color: Colors.white),
+        // Menentukan tema tampilan kalkulator
+        borderColor: Colors.black, // Warna border
+        borderWidth: 2, // Lebar border
+        displayColor: Colors.white, // Warna latar tampilan
+        expressionColor: Colors.indigo, // Warna ekspresi perhitungan
+        expressionStyle: TextStyle(fontSize: 80, color: Colors.yellow), // Gaya teks ekspresi
+        operatorColor: Colors.pink, // Warna operator (+, -, dll)
+        operatorStyle: TextStyle(fontSize: 30, color: Colors.white), // Gaya teks operator
+        commandColor: Colors.orange, // Warna tombol perintah (C, AC, dll)
+        commandStyle: TextStyle(fontSize: 30, color: Colors.white), // Gaya teks perintah
+        numColor: Colors.grey, // Warna tombol angka
+        numStyle: TextStyle(fontSize: 50, color: Colors.white), // Gaya teks angka
       ),
     );
+
+    // Mengembalikan OutlinedButton yang menampilkan nilai saat ini
     return OutlinedButton(
-      child: Text(_curentValue.toString()),
+      child: Text(_curentValue.toString()), // Teks pada tombol adalah nilai saat ini
       onPressed: () {
+        // Ketika tombol ditekan, menampilkan ModalBottomSheet dengan kalkulator
         showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (BuildContext context) {
-              return SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.75,
-                  child: calc);
-            });
+          isScrollControlled: true, // Mengizinkan layar untuk di-scroll saat keyboard muncul
+          context: context,
+          builder: (BuildContext context) {
+            // Mengatur tinggi kalkulator menjadi 75% dari tinggi layar
+            return SizedBox(
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: calc, // Menampilkan kalkulator
+            );
+          },
+        );
       },
     );
   }
-  // TODO: implement widget
-  CalcButton get widget => super.widget;
 
+  // Menambahkan getter untuk mengakses widget CalcButton
+  CalcButton get widget => super.widget;
 }
